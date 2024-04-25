d2b89994ac1e55df2f8acb045da1fa2565b8673c
Zimo Huang <zimoh@sas.upenn.edu>
2024-04-25 00:48:31 +0000

    fix chroma

--- server/app.js
+++ server/app.js
@@ -6,9 +6,8 @@ const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 
 var path = require('path');
-const { ChromaClient } = require("chromadb");
 const fs = require('fs');
-const chromadb = require('../models/chroma.js');
+const chromadb = require('./models/chroma.js');
 
 
 const session = require('express-session');
@@ -24,44 +23,11 @@ app.use(session({
 }));
 
 
+chromadb.initializeCollection().then(() => {
+    console.log('Collection initialized and ready to use.');
+});
+
+
 app.listen(port, () => {
-    console.log(`Main app listening on port ${port}`)
+    console.log(`Main app listening on port ${port}`);
 })
-
-
-// get chroma db running 
-const client = new ChromaClient();
-
-chromadb.initializeFaceModels()
-    .then(async () => {
-
-        const collection = await client.getOrCreateCollection({
-            name: "face-api",
-            embeddingFunction: null,
-            metadata: { "hnsw:space": "l2" },
-        });
-
-        console.info("Looking for files");
-        const promises = [];
-        // Loop through all the files in the images directory
-        fs.readdir("images", function (err, files) {
-            if (err) {
-                console.error("Could not list the directory.", err);
-                process.exit(1);
-            }
-
-            files.forEach(function (file, index) {
-                console.info("Adding task for " + file + " to index.");
-                promises.push(chromadb.indexAllFaces(path.join(__dirname, "..", "images", file), file, collection));
-            });
-            console.info("Done adding promises, waiting for completion.");
-            Promise.all(promises)
-                .then(async (results) => {
-                    console.info("All images indexed.");
-                })
-                .catch((err) => {
-                    console.error("Error indexing images:", err);
-                });
-        });
-
-    }); 
