248cedd5f80e5d602ec16c065711c33d02f1da3c
soniatam6 <107250031+soniatam6@users.noreply.github.com>
2024-05-01 11:45:22 -0400

    add Feed

---
+++ frontend/src/pages/Post.tsx
@@ -0,0 +1,15 @@
+import Navbar from '../components/Navigation';
+
+export default function Post() {
+    
+    
+    return (
+    <div className='w-screen h-screen flex flex-col items-center justify-start'>
+      <Navbar />
+      <div className='w-full max-w-[1800px] flex flex-col justify-center items-center space-y-8 p-8'>
+
+      </div>
+    </div>
+
+  );
+}
