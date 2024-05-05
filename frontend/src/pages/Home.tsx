02736b05815986082f613ee0df9f3b23daaf63b6
Zimo Huang <zimoh@sas.upenn.edu>
2024-05-05 16:43:03 -0400

    pull from main

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from 'axios';
 import config from '../../config.json';
 import { useParams, useNavigate } from 'react-router-dom';
