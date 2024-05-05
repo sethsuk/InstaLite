e481555a46ae8f6114abd95ceefe018b2280d609
Zimo Huang <zimoh@sas.upenn.edu>
2024-05-05 15:13:57 -0400

    logout button working

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -6,12 +6,14 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { styled, alpha } from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
+import axios from 'axios';
+import config from '../../config.json';
 
 interface props {
   username?: string
 }
 
-export default function Navigation({username} : props) {
+export default function Navigation({ username }: props) {
   const Search = styled('div')(({ theme }) => ({
     position: 'relative',
     borderRadius: theme.shape.borderRadius,
@@ -26,7 +28,7 @@ export default function Navigation({username} : props) {
       width: 'auto',
     },
   }));
-  
+
   const StyledInputBase = styled(InputBase)(({ theme }) => ({
     color: 'inherit',
     width: '100%',
@@ -42,24 +44,36 @@ export default function Navigation({username} : props) {
         },
       },
     },
-  })); 
-  const pages = ['Friends', 'Chat', 'Profile', 'Search', 'Logout'];
-  const navigate = useNavigate(); 
+  }));
+  const pages = ['Friends', 'Chat', 'Profile', 'Search'];
+  const navigate = useNavigate();
+
+  const rootURL = config.serverRootURL;
+
+  const handleLogout = async () => {
+    axios.defaults.withCredentials = true;
+    const response = await axios.post(`${rootURL}/${username}/logout`);
+    if (response.status === 200) {
+      alert('You are logged out.');
+      navigate('/login');
+    }
+  }
 
   return (
     <div className='w-screen space-y-24'>
-        <Box sx={{ flexGrow: 1 }}>
-          <AppBar position="static">
-            <Toolbar className='bg-slate-700'>
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} onClick={() => navigate(`/${username}/`)}>
-                Instalite
-              </Typography>
-                {pages.map((page) => (
-                  <Button color="inherit" onClick={() => navigate(`/${username}/${page.toLowerCase()}`)}>{page}</Button>
-                ))}
-            </Toolbar>
-          </AppBar>
-        </Box>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar className='bg-slate-700'>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} onClick={() => navigate(`/${username}/`)}>
+              Instalite
+            </Typography>
+            {pages.map((page) => (
+              <Button color="inherit" onClick={() => navigate(`/${username}/${page.toLowerCase()}`)}>{page}</Button>
+            ))}
+            <Button color="inherit" onClick={handleLogout}>Logout</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </div>
   )
 }
